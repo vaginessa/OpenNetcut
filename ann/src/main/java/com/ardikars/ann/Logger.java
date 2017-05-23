ee59ccfc8bf3edf2487d7340a865a4fb5ccf2c17
Ardika Rommy Sanjaya <ardikars@gmail.com>
2017-05-23 13:07:50 +0700

    23-05-2017 13:07:50 WIB

--- ann/src/main/java/com/ardikars/ann/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.ardikars.ann;
-
-import java.util.List;
-
-public interface Logger<T> {
-
-    public void next(T arg, Neuron neuron, List<Connection> connection, StringBuilder printble);
-    
-}
